b7464abb5f45a39b63ca454dcc0d41f42e9c849e
00kamil00 <kamilkalemba6@gmail.com>
2026-06-22 21:26:52 +0200

    feat: add getHumanChoice function

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function getComputerChoice() {
-    let number = Math.random()
+// function getComputerChoice() {
+//     let number = Math.random()
 
-    if (number <= 0.333) {
-        console.log("Rock")
-    } else if (number <= 0.666) { 
-        console.log("Paper")
+//     if (number <= 0.333) {
+//         console.log("Rock")
+//     } else if (number <= 0.666) { 
+//         console.log("Paper")
+//     } else {
+//         console.log("Scissors")
+//     }
+// }
+
+function getHumanChoice() {
+    let option = prompt("Choose rock, paper or scissors!")
+    if (option === "rock") {
+        return "rock"
+    } else if (option === "paper") {
+        return "paper"
+    } else if (option === "scissors") {
+        return "scissors"
     } else {
-        console.log("Scissors")
+        return "You choose the wrong option!"
     }
 }
 
-getComputerChoice()
+getHumanChoice()
